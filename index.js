0d0ea1e5ec5ad1d170dbbed0fb646731ba6b006c
Matheus Diógenes Amorim <matheusdiogenesdev@gmail.com>
2025-02-18 11:36:12 -0300

    atualizacoes postgresql

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import tarefasRoutes from './Routes/tarefasRoutes.js'
 
 const app = express();
 dotenv.config()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
